c0e4317233075ba4fee02b27c2080a4bd3726bd7
paulogustavo <paulogustavobarros90@gmail.com>
2025-02-21 17:38:45 -0300

    banco de dados alterado para o local de casa

--- src/database/index.js
+++ src/database/index.js
@@ -4,6 +4,6 @@ import Aluno from "../models/Aluno.js";
 const models = [Aluno];
 
 // const connection = new Sequelize(databaseConfig);
-const connection = new Sequelize('postgres://postgres:FantaUva10!@localhost:5432/Api-paulo'); // Example for postgres
+const connection = new Sequelize('postgres://postgres:FantaUva10!@localhost:5432/Escola'); // Example for postgres
 
 models.forEach(model => model.init(connection));
