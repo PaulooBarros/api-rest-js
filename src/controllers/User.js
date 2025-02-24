f6d4c86a45dbcbd27ca43bae41fa5255c38f3a74
paulo.barros <paulo.barros@protechsolutions.com.br>
2025-02-24 13:33:32 -0300

    tratamento de erro para post usuario

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,15 +1,24 @@
 import User from "../models/User.js";
 
 class UserController {
-  async store(req,res){
-    const novoUsuario = await User.create({
-      nome: "Paulo",
-      email: "paulo@email.com",
-      password: "@Test123"
-    });
-    res.json(novoUsuario);
+  async store(req, res) {
+    try {
+      const novoUsuario = await User.create({
+        nome: "Paulo",
+        email: "paulo@email.com",
+        password: "@Test123",
+      });
+      res.json(novoUsuario);
+    } catch (e) {
+      if (e.errors) {
+        console.log(e);
+        res.status(400).json(e.errors.map(erro => erro.message));
+      } else {
+        res.status(400).json({ error: e.message || "Erro desconhecido" });
+      }
+    }
+
   }
 }
-
 
 export default new UserController();
